9cad34bd14b9ec2fd945d7e9dd2ae3f6fc02e462
adityasharma2oo2 <adityasharma9319635@gmail.com>
2021-11-28 00:28:34 +0530

    Python Revision DAY 1

---
+++ bandnamegenerator.py
@@ -0,0 +1,7 @@
+print("Wecome to the band name generator!")
+
+city = input("Which city did you grow up in?\n")
+print(city)
+
+pet = input("What is Name of your pet\n")
+print ("you band name could be"p+ city+" "+pet)
---
+++ day_1.py
@@ -0,0 +1,25 @@
+#Print Function
+print("Hello!")
+
+
+#Input Function
+print("What is your Name?")
+
+#Name is a variable which store value by user.
+Name = input()
+ 
+# len is In-Built function which tell the length of string by counting number of character in it.
+print(len(Name))
+
+# Write a program of swapping value pof two variable 
+print("Enter the Value of Variable 1" )
+a = input()
+
+print("Enter the Value of Variable 2" )
+b = input()
+
+#(a,b) = (b,a) is the logic to swap to variable value
+(a,b) = (b, a)
+
+print("value of a is "+a)
+print("value of b is "+b)

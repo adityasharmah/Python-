7f6cd1d4f564f7429af0f92ef26d08ad16061b54
adityasharma2oo2 <adityasharma9319635@gmail.com>
2021-11-28 19:13:00 +0530

    done all today's topics

--- day_2.py
+++ day_2.py
@@ -3,3 +3,59 @@
 # Operations
 # Type conversion
 # f-strings
+
+# Hello[] istreat like str which have every chaeracter their index value (this index value start from  0)
+print('Hello'[0])
+print('Hello'[1])
+print('Hello'[2])
+print('Hello'[3])
+print('Hello'[4])
+
+print('Hello'[-1])
+
+print ("Hello " + "123" " World")
+
+# Integer
+# Intialize integeer datatype i am silply writing number 
+print(123 + 344)
+
+#Float
+pi =3.14159
+print(pi)
+
+# Boolean
+
+True
+False
+
+# type Function tell us which datatype is this 
+
+print(type(122))
+print(type("Aditya"))
+
+
+#code Challenge
+#Enter 2 digit number sting and print sum of both digit
+
+# Taking Input from user
+print("Enter a Number")
+n = input()
+
+# After divide into subscripting
+n1 = n[0]
+n2 = n[1]
+
+# then convert subcripting into integer type & print sum of both 
+print(int(n1) + int(n2)) 
+
+# Operator
+
+print("operators")
+print(2+3)
+print(9-2)
+print(2*3)
+print(8/2)
+print(2**3)
+
+print(4%3)
+
